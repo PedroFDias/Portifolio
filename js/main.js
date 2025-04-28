cedf68d361a4428b8af1a1df49b39fb9cba8fd47
PedroFDias <phf.dias.2022@gmail.com>
2025-04-28 11:58:57 -0300

    iniciar repositorio do portifólio

---
+++ js/main.js
@@ -0,0 +1,98 @@
+var typed = new Typed(".text", {
+    strings: ["Desenvolvedor de Software", "Estudande de Programação"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
+window.addEventListener('scroll', function () {
+    const header = document.getElementById('cabecalho');
+    if (window.scrollY > 50) {
+        header.classList.add('header-scrolled');
+    } else {
+        header.classList.remove('header-scrolled');
+    }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const projetos = document.getElementById('projetos');
+    const modal = document.getElementById('modal');
+    const titulo = document.getElementById('modal-title');
+    const descricao = document.getElementById('modal-description');
+    const btnClose = document.querySelector('.modal-close');
+    const carroselHTML = document.querySelector('#carouselExampleSlidesOnly .carousel-inner');
+
+    projetos.innerHTML = '';
+
+    let jsonProjetos = [];
+
+    fetch('./data/projetos.json')
+        .then(res => res.json())
+        .then(data => {
+            jsonProjetos = data;
+
+            let projTab = '';
+            jsonProjetos.forEach(p => {
+                projTab += `<div class="projeto" id="${p.id}">
+                                <h4>${p.titulo}</h4>
+                                <img class="casacidada" src="${p.images[0]}" alt="">
+                                <div class="linkIcons">
+                                    <a href="${p.link}"><i class="bi bi-github"></i></a>
+                                    <a href="#!" class="saiba-mais"><i class="bi bi-info-circle"></i></a>
+                                </div>
+                            </div>`;
+            })
+            projetos.innerHTML = projTab;
+
+            document.querySelectorAll('.saiba-mais').forEach(btn => {
+                btn.addEventListener('click', e => {
+                    e.preventDefault();
+
+                    const idCard = parseInt(btn.closest('.projeto').id, 10);
+                    const proj = jsonProjetos.find(p => p.id === idCard);
+
+                    const existingCarousel = bootstrap.Carousel.getInstance(document.getElementById('carouselExampleSlidesOnly'));
+                    if (existingCarousel) {
+                        existingCarousel.dispose(); // DESTROI a instância
+                    }
+
+                    let carrosel = `<div class="carousel-item active">
+                                    <img src="${proj.images[0]}" class="d-block w-100" alt="...">
+                                </div>`;
+                    for (let i = 1; i < proj.images.length; i++) {
+                        carrosel += `<div class="carousel-item">
+                                    <img src="${proj.images[i]}" class="d-block w-100" alt="...">
+                                </div>`;
+                    }
+
+                    carroselHTML.innerHTML = carrosel;
+                    titulo.textContent = proj.titulo;
+                    descricao.textContent = proj.descricao;
+
+                    modal.style.display = "flex";
+
+                    const carousel = new bootstrap.Carousel(document.getElementById('carouselExampleSlidesOnly'), {
+                        interval: 3000, // tempo entre slides (3 segundos)
+                        ride: 'carousel'
+                    });
+                })
+            })
+
+            btnClose.addEventListener("click", () => {
+                modal.style.display = "none";  // Esconde o modal
+            });
+
+            // Fecha o modal ao clicar fora do conteúdo
+            modal.addEventListener("click", e => {
+                if (e.target === modal) {
+                    modal.style.display = "none";  // Esconde o modal
+                }
+            });
+        })
+        .catch(err => console.error('Erro carregando projects.json:', err));
+
+
+})
